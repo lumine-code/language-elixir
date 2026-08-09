d2b78b2096af17f7b4ff5808de01b483fc9b5434
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-08-09 18:54:16 +0200

    Complete the Lumine ecosystem rebrand

--- spec/tree-sitter-grammar-spec.js
+++ spec/tree-sitter-grammar-spec.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 describe("Elixir Tree-sitter grammar", () => {
   beforeEach(async () => {
-    await atom.packages.activatePackage("language-elixir");
+    await lumine.packages.activatePackage("language-elixir");
   });
 
   it("tokenizes the fixture", async () => {
